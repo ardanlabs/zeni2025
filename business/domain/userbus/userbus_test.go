37959635305ed3d65db460d1e1a0400b3c4d8edf
William Kennedy <bill@ardanlabs.com>
2025-04-17 12:24:38 -0400

    commit 37

---
+++ business/domain/userbus/userbus_test.go
@@ -0,0 +1,153 @@
+package userbus_test
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/ardanlabs/service/business/domain/userbus"
+	"github.com/ardanlabs/service/business/sdk/dbtest"
+	"github.com/ardanlabs/service/business/sdk/page"
+	"github.com/ardanlabs/service/business/sdk/unitest"
+	"github.com/ardanlabs/service/business/types/role"
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_User(t *testing.T) {
+	t.Parallel()
+
+	db := dbtest.New(t, "Test_User")
+
+	sd, err := insertSeedData(db.BusDomain)
+	if err != nil {
+		t.Fatalf("Seeding error: %s", err)
+	}
+
+	// -------------------------------------------------------------------------
+
+	unitest.Run(t, query(db.BusDomain, sd), "query")
+	// unitest.Run(t, create(db.BusDomain), "create")
+	// unitest.Run(t, update(db.BusDomain, sd), "update")
+	// unitest.Run(t, delete(db.BusDomain, sd), "delete")
+}
+
+// =============================================================================
+
+func insertSeedData(busDomain dbtest.BusDomain) (unitest.SeedData, error) {
+	ctx := context.Background()
+
+	usrs, err := userbus.TestSeedUsers(ctx, 2, role.Admin, busDomain.User)
+	if err != nil {
+		return unitest.SeedData{}, fmt.Errorf("seeding users : %w", err)
+	}
+
+	tu1 := unitest.User{
+		User: usrs[0],
+	}
+
+	tu2 := unitest.User{
+		User: usrs[1],
+	}
+
+	// -------------------------------------------------------------------------
+
+	usrs, err = userbus.TestSeedUsers(ctx, 2, role.User, busDomain.User)
+	if err != nil {
+		return unitest.SeedData{}, fmt.Errorf("seeding users : %w", err)
+	}
+
+	tu3 := unitest.User{
+		User: usrs[0],
+	}
+
+	tu4 := unitest.User{
+		User: usrs[1],
+	}
+
+	// -------------------------------------------------------------------------
+
+	sd := unitest.SeedData{
+		Users:  []unitest.User{tu3, tu4},
+		Admins: []unitest.User{tu1, tu2},
+	}
+
+	return sd, nil
+}
+
+// =============================================================================
+
+func query(busDomain dbtest.BusDomain, sd unitest.SeedData) []unitest.Table {
+	usrs := make([]userbus.User, 0, len(sd.Admins)+len(sd.Users))
+
+	for _, adm := range sd.Admins {
+		usrs = append(usrs, adm.User)
+	}
+
+	for _, usr := range sd.Users {
+		usrs = append(usrs, usr.User)
+	}
+
+	sort.Slice(usrs, func(i, j int) bool {
+		return usrs[i].ID.String() <= usrs[j].ID.String()
+	})
+
+	// -------------------------------------------------------------------------
+
+	cmp := func(got any, exp any) string {
+		gotResp, exists := got.(userbus.User)
+		if !exists {
+			return "error occurred"
+		}
+
+		expResp := exp.(userbus.User)
+
+		if gotResp.DateCreated.Format(time.RFC3339) == expResp.DateCreated.Format(time.RFC3339) {
+			expResp.DateCreated = gotResp.DateCreated
+		}
+
+		if gotResp.DateUpdated.Format(time.RFC3339) == expResp.DateUpdated.Format(time.RFC3339) {
+			expResp.DateUpdated = gotResp.DateUpdated
+		}
+
+		return cmp.Diff(gotResp, expResp)
+	}
+
+	// -------------------------------------------------------------------------
+
+	table := []unitest.Table{
+		{
+			Name:    "all",
+			ExpResp: usrs,
+			ExcFunc: func(ctx context.Context) any {
+				filter := userbus.QueryFilter{
+					Name: dbtest.NamePointer("Name"),
+				}
+
+				resp, err := busDomain.User.Query(ctx, filter, userbus.DefaultOrderBy, page.MustParse("1", "10"))
+				if err != nil {
+					return err
+				}
+
+				return resp
+			},
+			CmpFunc: cmp,
+		},
+		{
+			Name:    "byid",
+			ExpResp: sd.Users[0].User,
+			ExcFunc: func(ctx context.Context) any {
+				resp, err := busDomain.User.QueryByID(ctx, sd.Users[0].ID)
+				if err != nil {
+					return err
+				}
+
+				return resp
+			},
+			CmpFunc: cmp,
+		},
+	}
+
+	return table
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.26.0
+	github.com/google/go-cmp v0.3.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/jmoiron/sqlx v1.4.0
